15dca51582626706912253bf24984ecba0c5266f
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-04-25 13:43:33 +0300

    fix: update footer copyright display to show current year dynamically

--- frontend/src/components/ui/footer.tsx
+++ frontend/src/components/ui/footer.tsx
@@ -1,12 +1,14 @@
 import { Copyright } from "lucide-react";
 import { ModeToggle } from "../dark-mode/mode-toggle";
 
-
 export const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className="w-full relative mt-auto py-4">
             <p className="mx-auto px-4 py-1 mb-2 rounded-full bg-muted w-fit text-xs flex items-center justify-center gap-x-2 text-muted-foreground">
-                <Copyright className="inline size-3" /> 2023 TASKFLOW. All rights reserved.
+                <Copyright className="inline size-3" />
+                {currentYear} TASKFLOW - All rights reserved.
             </p>
             <div className="absolute bottom-4 right-4">
                 <ModeToggle />
