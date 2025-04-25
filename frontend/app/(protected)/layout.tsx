b9f85ebf05da52cede34d9dbd270958163437575
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-04-25 11:55:22 +0300

    refactor: move ModeToggle to footer

--- frontend/app/(protected)/layout.tsx
+++ frontend/app/(protected)/layout.tsx
@@ -1,12 +1,10 @@
-import Header from "@/components/ui/header";
+import { Header } from "@/components/ui/header";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="w-full flex-grow flex flex-col">
             <Header />
-            <main className="w-11/12 my-4 mx-auto flex-grow">
-                {children}
-            </main>
+            <main className="w-11/12 my-4 mx-auto flex-grow">{children}</main>
         </div>
     );
 }
